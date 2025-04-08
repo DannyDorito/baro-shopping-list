acfd38cbb06832aa9addf683661fe36808dfe65d
John Allison <john@jallison.co.uk>
2025-04-08 16:05:56 +0100

    Update Feedback.tsx

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { set, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -15,7 +15,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Toaster } from "@/components/ui/sonner";
 import { toast } from "sonner";
 import { useState } from "react";
 import { Loader2, Send } from "lucide-react";
